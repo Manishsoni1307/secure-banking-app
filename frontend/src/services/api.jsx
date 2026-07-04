485606463436feb43e0599a9aed28e735b264321
Manish Soni <thisismanishsoni1307.ms@gmail.com>
2026-07-04 09:51:37 +0530

    fix: update frontend environment url variables

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -3,14 +3,14 @@ import axios from "axios";
 console.log("API URL =", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://secure-banking-app-2.onrender.com/api",
+
+  baseURL: "https://banking-app-backend-h2gp.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+
 
 API.interceptors.request.use(
   (config) => {
