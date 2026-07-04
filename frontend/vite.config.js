143f0242dd1bdab093b056209b2e0d37f5003497
Manish Soni <thisismanishsoni1307.ms@gmail.com>
2026-07-04 20:19:08 +0530

    chore: add code splitting to resolve chunk size warning

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Splits third-party node_modules packages into a separate 'vendor' bundle
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
